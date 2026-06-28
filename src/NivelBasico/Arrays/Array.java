8a091d1da30d40c9256554409147d8de82703c62
arlei duarte <arleidt@yahoo.com.br>
2026-06-27 21:55:16 -0300

    arrays

--- src/NivelBasico/Arrays/Array.java
+++ src/NivelBasico/Arrays/Array.java
@@ -3,19 +3,34 @@ package NivelBasico.Arrays;
 public class Array {
     public static void main(String[] args) {
         /*
-        String ninja1 = "naruto";
-        String ninja2 = "sasuke";
-        String ninja3 = "sakura";
-        System.out.println(ninja1);
-        System.out.println(ninja2);
-        System.out.println(ninja3);
+
         inicializando array mas da tb da outra forma que é boa pratica em java String[]
         String ninja[];
+        array e lista em java sao diferentes, array tem tipo definido e sabe exatamente, o tamanho dados simples ja lista se extende sozinho
+        arrays sao tipos referencia. array inicializa como null
          */
-        String[] ninja = new String[3]; //criou 3 espaços
+        String[] ninja = new String[4]; //criou 3 espaços
         ninja[0] = "Naruto";
         ninja[1] = "Sasuke";
         ninja[2] = "Sakura";
-        System.out.println(ninja);
+        System.out.println(ninja[3]);
+
+        //array idade inicializa 0
+        int[] idade = new int[2];
+        idade[0]= 16;
+        System.out.println(idade[1]);
+
+        // inicializa como false
+        boolean[] verdadeiroOuFalso = new boolean[2];
+        verdadeiroOuFalso[0]= true;
+        System.out.println(verdadeiroOuFalso[1]);
+        //inicializa como 0.0
+        double[] flutuante = new double[1];
+        System.out.println(flutuante[0]);
+        //inicializa como 0.0
+        float[]flutuante2 = new float[1];
+        System.out.println(flutuante2[0]);
+
+
     }
 }
