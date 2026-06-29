fc527391ed513e8ac20518264368a74421455f9f
arlei duarte <arleidt@yahoo.com.br>
2026-06-29 13:29:48 -0300

    Desafios

---
+++ src/teste.java
@@ -0,0 +1,6 @@
+public class teste {
+    public static void main(String[] args) {
+        System.out.println("\n===== Menu Ninja =====");
+        System.out.println("1. Cadastrar Ninja");
+    }
+}
